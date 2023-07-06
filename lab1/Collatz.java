a932a4697e01d54ffb95780952c6a93772b32f5c
join <2208086854@qq.com>
2023-07-06 16:52:26 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,13 +5,22 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        /** if (n  == 128) {
             return 1;
         } else if (n == 5) {
             return 3 * n + 1;
         } else {
             return n * 2;
+        } */
+        if(n==1){
         }
+        else if(n %2==0){
+            n=n/2;
+        }
+        else{
+            n=n*3+1;
+        }
+        return n;
     }
 
     public static void main(String[] args) {
@@ -21,7 +30,7 @@ public class Collatz {
             n = nextNumber(n);
             System.out.print(n + " ");
         }
-        System.out.println();
+        System.out.println(" ");
     }
 }
 
